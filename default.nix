3de827b97d11379a6e18b3ece16796ee9a528e8a
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2020-04-27 11:29:35 +0100

    initial commit of nix-based testbed configuration

---
+++ default.nix
@@ -0,0 +1,32 @@
+argsOuter@{...}:
+let
+  # specifying args defaults in this slightly non-standard way to allow us to include the default values in `args`
+  args = rec {
+    pkgs = import <nixpkgs> {};
+    localOverridesPath = ./local.nix;
+  } // argsOuter;
+  rehomeNginx = import ./rehome-nginx.nix;
+in (with args; {
+  ckanMockHarvestSourceEnv = (pkgs.stdenv.mkDerivation rec {
+      name = "ckan-mock-harvest-source-env";
+      shortName = "ck-mk-hvst-src";
+      buildInputs = [
+        (rehomeNginx {
+          inherit (pkgs) stdenv writeText symlinkJoin makeWrapper;
+          nginx = pkgs.nginx;
+          sitesDir = (toString (./.)) + "/sites";
+          logDir = (toString (./.)) + "/nix-var/logs";
+          runDir = (toString (./.)) + "/nix-var/run";
+          responsesDir = (toString (./.)) + "/responses";
+        })
+      ];
+
+      # if we don't have this, we get unicode troubles in a --pure nix-shell
+      LANG="en_GB.UTF-8";
+
+      shellHook = ''
+        export PS1="\[\e[0;36m\](nix-shell\[\e[0m\]:\[\e[0;36m\]${shortName})\[\e[0;32m\]\u@\h\[\e[0m\]:\[\e[0m\]\[\e[0;36m\]\w\[\e[0m\]\$ "
+      '';
+    }
+  ).overrideAttrs (if builtins.pathExists localOverridesPath then (import localOverridesPath args) else (x: x));
+})
---
+++ rehome-nginx.nix
@@ -0,0 +1,51 @@
+{ stdenv, symlinkJoin, writeText, makeWrapper, nginx, sitesDir, logDir, runDir, responsesDir}:
+
+symlinkJoin {
+  name = "rehomed-nginx";
+  paths = [ nginx ];
+  nativeBuildInputs = [ makeWrapper ];
+  # this nginx.conf indended to have minimal variation from nginx docker image's default
+  nginxConf = writeText "nginx.conf" ''
+    worker_processes  1;
+
+    error_log  ${logDir}/error.log debug;
+    pid        ${runDir}/nginx.pid;
+
+    events {
+        worker_connections  1024;
+    }
+
+    http {
+        map $host $mock_ckan_responses_root { default ${responsesDir}; }
+        include       mime.types;
+        default_type  application/octet-stream;
+
+        log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                          '$status $body_bytes_sent "$http_referer" '
+                          '"$http_user_agent" "$http_x_forwarded_for" "$realpath_root" "$request_uri" "$uri"';
+
+        access_log  ${logDir}/access.log  main;
+        client_body_temp_path ${runDir}/cbtmp;
+        proxy_temp_path ${runDir}/ptmp;
+        fastcgi_temp_path ${runDir}/ftmp;
+        uwsgi_temp_path ${runDir}/utmp;
+        scgi_temp_path ${runDir}/stmp;
+
+        sendfile        on;
+        #tcp_nopush     on;
+
+        keepalive_timeout  65;
+
+        #gzip  on;
+
+        include ${sitesDir}/*.conf;
+    }
+  '';
+  postBuild = ''
+    rm $out/conf/nginx.conf
+    ln -s $nginxConf $out/conf/nginx.conf
+    wrapProgram $out/bin/nginx \
+      --run "mkdir -p ${sitesDir} ${logDir} ${runDir} ${responsesDir}" \
+      --add-flags "-c $out/conf/nginx.conf"
+  '';
+}
